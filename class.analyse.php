48a441a6f2fe39b5f2904b6d295e1d7a06539e1b
mgosse <mathieu.gosse@forlabs.fr>
2022-10-06 14:54:07 +0200

    retriver

---
+++ class.analyse.php
@@ -0,0 +1,13 @@
+<?php
+include("class.algorithm.php");
+
+class analyse{
+    
+    public function __constructor(){
+        //
+    };
+    
+    public function getAlgoInstruction($trader){
+        
+    };
+}
