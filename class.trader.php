25b939ea17f0c803d4f3996c8871abbc9c2839f8
mgosse <mathieu.gosse@forlabs.fr>
2022-10-07 10:54:44 +0200

    interface trader & binancetrader

--- class.trader.php
+++ class.trader.php
@@ -1,30 +1,7 @@
 <?php
-include('class.wallet.php');
-
-
-
-
-class Trader{
-    public $bank;
-
-    public function __constructor($bank){
-        $this->bank = $bank;
-    }
-
-    public function buyCrypto($wallet){
-        #To do callback to Bank
-        $wallet->storeMoney(1000);
-        $wallet->storeCryptos(1000);
-    }
-
-    public function sellCrypto($wallet){
-        #To do callback to Bank
-        $wallet->withdrawMoney(1000);
-        $wallet->withdrawACrypto(1000);
-    }
-
+interface Trader{    
+    public function buyCrypto($wallet):void;
+    public function sellCrypto($wallet):void; 
 };
 // To moove
-$wallet = new Wallet();
-$trader = new Trader('banque');
 // 
